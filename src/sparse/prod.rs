2b3c99acccf92795a1d9bee1014d55ca47987ea7
Vincent Barrielle <vincent.barrielle@m4x.org>
2015-01-13 19:21:14 +0100

    added a test for CSC/vector multiplication

--- src/sparse/prod.rs
+++ src/sparse/prod.rs
@@ -19,3 +19,34 @@ pub fn mul_acc_mat_vec_csc<N: Add<Output=N> + Mul<Output=N> + Clone + Copy>(
         }
     }
 }
+
+
+#[cfg(test)]
+mod test {
+    use sparse::csmat::{new_borrowed_csmat};
+    use sparse::csmat::CompressedStorage::{CSC, CSR};
+    use super::mul_acc_mat_vec_csc;
+    use std::num::Float;
+
+    #[test]
+    fn mul_csc_vec() {
+        let indptr: &[usize] = &[0, 2, 4, 5, 6, 7];
+        let indices: &[usize] = &[2, 3, 3, 4, 2, 1, 3];
+        let data: &[f64] = &[
+            0.35310881, 0.42380633, 0.28035896, 0.58082095,
+            0.53350123, 0.88132896, 0.72527863];
+
+        let mat = new_borrowed_csmat(CSC, 5, 5, indptr, indices, data).unwrap();
+        let vector = vec![0.1, 0.2, -0.1, 0.3, 0.9];
+        let mut resVec = vec![0., 0., 0., 0., 0.];
+        mul_acc_mat_vec_csc(mat, vector.as_slice(), resVec.as_mut_slice());
+
+        let expected_output =
+            vec![ 0., 0.26439869, -0.01803924, 0.75120319, 0.11616419];
+
+        let epsilon = 1e-7; // TODO: get better values and increase precision
+
+        assert!(resVec.iter().zip(expected_output.iter()).all(
+            |(x,y)| Float::abs(*x-*y) < epsilon));
+    }
+}
