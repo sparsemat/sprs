fe1073ffd8441afc72201c33ec7ccd37f8b222f6
Vincent Barrielle <vincent.barrielle@m4x.org>
2015-02-06 18:29:20 +0100

    added test for is_symmetric

--- src/sparse/symmetric.rs
+++ src/sparse/symmetric.rs
@@ -19,3 +19,43 @@ pub fn is_symmetric<N: Clone + Copy + PartialEq>(mat: &CsMat<N>) -> bool {
     }
     true
 }
+
+#[cfg(test)]
+mod test {
+    use sparse::csmat::new_borrowed_csmat;
+    use sparse::csmat::CompressedStorage::{CSC, CSR};
+    use super::is_symmetric;
+
+    #[test]
+    fn is_symmetric_simple() {
+        let indptr : &[usize] = &[0, 2, 5, 6, 7, 13, 14, 17, 20, 24, 28];
+        let indices : &[usize] = &[
+            0, 8,
+            1, 4, 9,
+            2,
+            3,
+            1, 4, 6, 7, 8, 9,
+            5,
+            4, 6, 9,
+            4, 7, 8,
+            0, 4, 7, 8,
+            1, 4, 6, 9];
+        let data : &[f64] = &[
+            1.7, 0.13,
+            1., 0.02, 0.01,
+            1.5,
+            1.1,
+            0.02, 2.6, 0.16, 0.09, 0.52, 0.53,
+            1.2,
+            0.16, 1.3, 0.56,
+            0.09, 1.6, 0.11,
+            0.13, 0.52, 0.11, 1.4,
+            0.01, 0.53, 0.56, 3.1];
+
+        let a = new_borrowed_csmat(CSR, 10, 10, indptr, indices, data).unwrap();
+
+        assert!(is_symmetric(&a));
+    }
+
+    // TODO: symmetry test on A^T*A products
+}
