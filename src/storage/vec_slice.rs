97af91bac32957c180cbdf1fa43e4942935983f8
Vincent Barrielle <vincent.barrielle@m4x.org>
2015-02-21 19:30:17 +0100

    Add support for mutability in the storage structure
    There is a runtime panic though if mutability has been tried on
    a storage that was in fact an immutable slice, maybe there's a way
    to check this at compile time.

--- src/storage/vec_slice.rs
+++ src/storage/vec_slice.rs
@@ -5,11 +5,12 @@
 /// (ie a submatrix extracted from a matrix should borrow the data but be of
 /// the same type).
 
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 
 pub enum VecSlice<'a, N: 'a> {
     Vec(Vec<N>),
-    Slice(&'a[N])
+    Slice(&'a[N]),
+    MutSlice(&'a mut[N])
 }
 
 impl<'a, N: 'a> VecSlice<'a, N> {
@@ -20,6 +21,10 @@ impl<'a, N: 'a> VecSlice<'a, N> {
     pub fn from_slice(s: &'a[N]) -> VecSlice<'a, N> {
         VecSlice::Slice(s)
     }
+
+    pub fn from_mut_slice(s: &'a mut[N]) -> VecSlice<'a, N> {
+        VecSlice::MutSlice(s)
+    }
 }
 
 impl<'a, N> Deref for VecSlice<'a, N> {
@@ -28,7 +33,18 @@ impl<'a, N> Deref for VecSlice<'a, N> {
     fn deref(& self) -> &[N] {
         match self {
             &VecSlice::Vec(ref v) => &v[..],
-            &VecSlice::Slice(s) => s
+            &VecSlice::Slice(s) => s,
+            &VecSlice::MutSlice(ref s) => s
+        }
+    }
+}
+
+impl<'a, N> DerefMut for VecSlice<'a, N> {
+    fn deref_mut(& mut self) -> &mut[N] {
+        match self {
+            & mut VecSlice::Vec(ref mut v) => v.as_mut_slice(),
+            & mut VecSlice::Slice(mut s) => panic!("not mutable"), // FIXME
+            & mut VecSlice::MutSlice(ref mut s) => s
         }
     }
 }
@@ -49,5 +65,27 @@ mod test {
         let b = vs_slice[0];
 
         assert!(a == b); // just to avoid warnings
+    }
+
+    #[test]
+    fn deref_mut() {
+        let v = vec![0, 1, 2];
+        let mut vs_vec = VecSlice::from_vec(v);
+        let mut s = [0,1,2];
+        let mut vs_slice_mut = VecSlice::from_mut_slice(&mut s);
+
+        vs_vec[0] = 1;
+        assert!(vs_vec[0] == 1);
+        vs_slice_mut[0] = 1;
+        assert!(vs_slice_mut[0] == 1);
+
+    }
+
+    #[test]
+    #[should_fail]
+    fn deref_mut_panics_on_immutable_slice() {
+        let s = [0,1,2];
+        let mut vs_slice = VecSlice::from_slice(&s);
+        vs_slice[0] = 1;
     }
 }
