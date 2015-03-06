0c6096601a72aeb283e815f95fdbb81d9c654023
Vincent Barrielle <vincent.barrielle@m4x.org>
2015-03-06 08:51:16 +0100

    more flexible low level in cholesky, WIP

--- src/sparse/linalg/cholesky.rs
+++ src/sparse/linalg/cholesky.rs
@@ -30,9 +30,12 @@ pub enum SymmetryCheck {
 /// Perform a symbolic LDLt decomposition of a symmetric sparse matrix
 pub fn ldl_symbolic<N, IStorage, DStorage, PStorage>(
     mat: &CsMat<N, IStorage, DStorage>,
-    perm: Permutation<PStorage>,
-    flag: &mut [usize],
-    check_symmetry: SymmetryCheck) -> Option<SymbolicLDL>
+    perm: &Permutation<PStorage>,
+    l_colptr: &mut [usize],
+    parents: &mut [isize],
+    l_nz: &mut [usize],
+    flag_workspace: &mut [usize],
+    check_symmetry: SymmetryCheck) -> Result<(),()>
 where
 N: Clone + Copy + PartialEq,
 IStorage: Deref<Target=[usize]>,
@@ -42,7 +45,7 @@ PStorage: Deref<Target=[usize]> {
     match check_symmetry {
         SymmetryCheck::DontCheckSymmetry => (),
         SymmetryCheck::CheckSymmetry => if ! is_symmetric(mat) {
-            return None;
+            return Err(());
         }
     }
 
@@ -53,7 +56,7 @@ PStorage: Deref<Target=[usize]> {
 
     for (k, (outer_ind, vec)) in mat.outer_iterator_papt(&perm.borrowed()).enumerate() {
 
-        flag[k] = k; // this node is visited
+        flag_workspace[k] = k; // this node is visited
 
         for (inner_ind, _) in vec.iter() {
             let mut i = inner_ind;
@@ -64,12 +67,12 @@ PStorage: Deref<Target=[usize]> {
             if i < outer_ind {
                 // get back to the root of the etree
                 // TODO: maybe this calls for a more adequate parent structure?
-                while flag[i] != outer_ind {
+                while flag_workspace[i] != outer_ind {
                     if parents[i] == -1 {
                         parents[i] = outer_ind as isize; // TODO check overflow
                     }
                     l_nz[i] = l_nz[i] + 1;
-                    flag[i] = outer_ind;
+                    flag_workspace[i] = outer_ind;
                     i = parents[i] as usize; // TODO check negative
                 }
             }
@@ -84,56 +87,44 @@ PStorage: Deref<Target=[usize]> {
         l_colptr.push(prev);
     }
 
-    Some(SymbolicLDL {
-        n: n,
-        l_colptr: l_colptr,
-        l_nz: l_nz,
-        parents: parents,
-        perm: perm.owned_clone()
-    })
+    Ok(())
 }
 
-pub struct Ldlt<N> {
-    l_nz: &[usize],
-    l_indices: &[usize],
-    l_data: &[N],
-    d: &[N]
-}
-
-pub fn ldl_numeric<N, IStorage, DStorage>(
+pub fn ldl_numeric<N, IStorage, DStorage, PStorage>(
+    n: usize,
     mat: &CsMat<N, IStorage, DStorage>,
-    ldl_sym: &SymbolicLDL,
+    l_colptr: &[usize],
+    parents: &[isize],
+    perm: &Permutation<PStorage>,
+    l_nz: &mut [usize],
+    l_indices: &mut [usize],
+    l_data: &mut [N],
+    diag: &mut [N],
     y_workspace: &mut [N],
     pattern_workspace: &mut [usize],
     flag_workspace: &mut [usize]) -> Result<(), usize>
 where
-N: Clone + Copy + PartialEq + Num,
+N: Clone + Copy + PartialEq + Num + PartialOrd,
 IStorage: Deref<Target=[usize]>,
 DStorage: Deref<Target=[N]>,
 PStorage: Deref<Target=[usize]> {
-    panic!("not yet implemented");
-    let n = ldl_sym.n;
-    let parents = &ldl_sym.parents;
-    let l_colptr = &ldl_sym.l_colptr;
-    let mut l_nz = vec![0usize, n]; // FIXME: should leave allocation to caller
-    let mut l_indices = vec![0usize, n]; // FIXME: should leave allocation to caller
-    let mut diag = vec![N::zero(), n];
 
-    for (k, (outer_ind, vec)) in mat.outer_iterator_papt(&perm.borrowed()).enumerate() {
+    for (k, (outer_ind, vec))
+    in mat.outer_iterator_papt(&perm.borrowed()).enumerate() {
 
         // compute the nonzero pattern of the kth row of L
         // in topological order
 
-        flag[k] = k; // this node is visited
+        flag_workspace[k] = k; // this node is visited
         y_workspace[k] = N::zero();
         l_nz[k] = 0;
         let mut top = n;
 
-        for (inner_ind, val) in vec.iter().filter(|i| i <= k) {
-            Y[inner_ind] += val;
+        for (inner_ind, val) in vec.iter().filter(|&(i,_)| i <= k) {
+            y_workspace[inner_ind] += val;
             let mut i = inner_ind;
             let mut len = 0;
-            while flag[i] != outer_ind {
+            while flag_workspace[i] != outer_ind {
                 pattern_workspace[len] = i;
                 len += 1;
                 flag_workspace[i] = k;
@@ -150,7 +141,7 @@ PStorage: Deref<Target=[usize]> {
         // of the kth row of L
         diag[k] = y_workspace[k];
         y_workspace[k] = N::zero();
-        'pattern: for i in pattern[top..n] {
+        'pattern: for i in &pattern_workspace[top..n] {
             let yi = y_workspace[i];
             y_workspace[i] = N::zero();
             let p2 = l_colptr[i] + l_nz[i];
@@ -161,12 +152,12 @@ PStorage: Deref<Target=[usize]> {
                 // actually each iteration of the 'pattern loop adds writes the
                 // value in l_indices that will be read on the next iteration
                 // TODO: can some design change make this fact more obvious?
-                Y[l_indices[p]] -= l_data[p] * yi;
+                y_workspace[l_indices[p]] -= l_data[p] * yi;
             }
             let l_ki = yi / diag[i];
             diag[k] -= l_ki * yi;
-            l_indices[p] = k;
-            l_data[p] = l_ki;
+            l_indices[p2] = k;
+            l_data[p2] = l_ki;
             l_nz[i] += 1;
         }
         if diag[k] == N::zero() {
