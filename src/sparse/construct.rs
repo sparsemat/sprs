39d0219896497080bc74b63695d11ea4d374b070
Vincent Barrielle <vincent.barrielle@m4x.org>
2015-07-20 08:21:09 +0200

    test fast track stack

--- src/sparse/construct.rs
+++ src/sparse/construct.rs
@@ -47,13 +47,20 @@ mod test {
     use test_data::{mat1, mat2, mat3, mat4};
     use errors::SprsError::*;
 
+    fn mat1_vstack_mat2() -> CsMat<f64, Vec<usize>, Vec<f64>> {
+        let indptr = vec![0, 2, 4, 5, 6, 7, 11, 13, 13, 15, 17];
+        let indices = vec![2, 3, 3, 4, 2, 1, 3, 0, 1, 2, 4, 0, 3, 2, 3, 1, 2];
+        let data = vec![3., 4., 2., 5., 5., 8., 7., 6., 7., 3., 3.,
+                        8., 9., 2., 4., 4., 4.];
+        CsMat::from_vecs(CSR, 10, 5, indptr, indices, data).unwrap()
+    }
+
     #[test]
     fn same_storage_fast_stack_failures() {
         let res: Result<CsMat<f64, _, _>, _> =
             super::same_storage_fast_stack(&[]);
         assert_eq!(res, Err(EmptyStackingList));
         let a = mat1();
-        let b = mat2();
         let c = mat3();
         let d = mat4();
         let res: Result<CsMat<f64, _, _>, _> =
@@ -62,5 +69,14 @@ mod test {
         assert_eq!(res, Err(IncompatibleDimensions));
         let res = super::same_storage_fast_stack(&[a.borrowed(), d.borrowed()]);
         assert_eq!(res, Err(IncompatibleStorages));
+    }
+
+    #[test]
+    fn same_storage_fast_stack_ok() {
+        let a = mat1();
+        let b = mat2();
+        let res = super::same_storage_fast_stack(&[a.borrowed(), b.borrowed()]);
+        let expected = mat1_vstack_mat2();
+        assert_eq!(res, Ok(expected));
     }
 }
