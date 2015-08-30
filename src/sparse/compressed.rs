935672caab9692de2870d613ace1d90e858d2bfd
Vincent Barrielle <vincent.barrielle@m4x.org>
2015-08-30 18:32:58 +0200

    first draft of Add implementation for CsMat

--- src/sparse/compressed.rs
+++ src/sparse/compressed.rs
@@ -1,8 +1,10 @@
 ///! Traits to generalize over compressed sparse matrices storages
 
 
-use sparse::csmat::{CsMat, CsMatView};
-use std::ops::{Deref};
+use sparse::csmat::{CsMat, CsMatVec, CsMatView};
+use sparse::binop;
+use num::traits::Num;
+use std::ops::{Deref, Add};
 
 /// The SpMatView trait describes data that can be seen as a view
 /// into a CsMat
@@ -20,5 +22,22 @@ where N: Copy,
 
     fn borrowed(&self) -> CsMatView<N> {
         self.borrowed()
+    }
+}
+
+impl<'a, 'b, N, IStorage, DStorage, Mat> Add<&'b Mat>
+for &'a CsMat<N, IStorage, DStorage>
+where N: 'a + Copy + Num + Default,
+      IStorage: 'a + Deref<Target=[usize]>,
+      DStorage: 'a + Deref<Target=[N]>,
+      Mat: SpMatView<N> {
+    type Output = CsMatVec<N>;
+
+    fn add(self, rhs: &'b Mat) -> CsMatVec<N> {
+        if self.storage() != rhs.borrowed().storage() {
+            return binop::add_mat_same_storage(
+                self, &rhs.borrowed().to_other_storage()).unwrap()
+        }
+        binop::add_mat_same_storage(self, rhs).unwrap()
     }
 }
