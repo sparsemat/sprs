345e5984af27d4ec8bd9714ac1df9d7ac138db6e
Vincent Barrielle <vincent.barrielle@m4x.org>
2015-09-02 20:41:20 +0200

    add tests for all cases of Mul impl in matrix-vector products

--- src/sparse/prod.rs
+++ src/sparse/prod.rs
@@ -280,4 +280,37 @@ mod test {
                                                vec![3., 5., 5.]);
         assert_eq!(expected_output, res);
     }
+
+    #[test]
+    fn mul_csvec_csr() {
+        let a = mat1();
+        let v = CsVec::new_owned(5, vec![0, 2, 4], vec![1.; 3]);
+        let res = &v * &a;
+        let expected_output = CsVec::new_owned(5,
+                                               vec![2, 3],
+                                               vec![8., 11.]);
+        assert_eq!(expected_output, res);
+    }
+
+    #[test]
+    fn mul_csc_csvec() {
+        let a = mat1_csc();
+        let v = CsVec::new_owned(5, vec![0, 2, 4], vec![1.; 3]);
+        let res = &a * &v;
+        let expected_output = CsVec::new_owned(5,
+                                               vec![0, 1, 2],
+                                               vec![3., 5., 5.]);
+        assert_eq!(expected_output, res);
+    }
+
+    #[test]
+    fn mul_csvec_csc() {
+        let a = mat1_csc();
+        let v = CsVec::new_owned(5, vec![0, 2, 4], vec![1.; 3]);
+        let res = &v * &a;
+        let expected_output = CsVec::new_owned(5,
+                                               vec![2, 3],
+                                               vec![8., 11.]);
+        assert_eq!(expected_output, res);
+    }
 }
