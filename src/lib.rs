7cedbc72c58763d856b9aca36efb68dc97f9867f
Vincent Barrielle <vincent.barrielle@m4x.org>
2015-09-08 08:16:58 +0200

    add some top level doc examples

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,47 @@
 
 sprs is a sparse linear algebra library for Rust.
 
+It features a sparse matrix type, CsMat, and a sparse vector type, CsVec,
+both based on the compressed storage scheme.
+
+All matrix algebra operations are supported, and support for direct sparse
+solvers is planned.
+
+## Examples
+
+Matrix construction
+
+```rust
+use sprs::{CsMat, CsMatVec, CsVec};
+let eye : CsMatVec<f64> = CsMat::eye(sprs::CSR, 3);
+let a = CsMat::from_vecs(sprs::CSC, 3, 3,
+                         vec![0, 2, 4, 5],
+                         vec![0, 1, 0, 2, 2],
+                         vec![1., 2., 3., 4., 5.]).unwrap();
+```
+
+Matrix vector multiplication
+
+```rust
+use sprs::{CsMat, CsVec};
+let eye = CsMat::eye(sprs::CSR, 5);
+let x = CsVec::new_owned(5, vec![0, 2, 4], vec![1., 2., 3.]).unwrap();
+let y = &eye * &x;
+assert_eq!(x, y);
+```
+
+Matrix matrix multiplication, addition
+```rust
+use sprs::{CsMat, CsVec};
+let eye = CsMat::eye(sprs::CSR, 3);
+let a = CsMat::from_vecs(sprs::CSC, 3, 3,
+                         vec![0, 2, 4],
+                         vec![0, 1, 0, 2, 2],
+                         vec![1., 2., 3., 4., 5.]).unwrap();
+let b = &eye * &a:
+assert_eq!(a, b);
+```
+
 */
 
 extern crate num;
