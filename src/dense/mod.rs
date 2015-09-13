7ea6f9c6c600da7e971c890e410edb1ad80c07a6
Vincent Barrielle <vincent.barrielle@m4x.org>
2015-09-13 20:34:38 +0200

    implement row/col slicing for dense matrices, and dense vec iteration

--- src/dense/mod.rs
+++ src/dense/mod.rs
@@ -1,9 +1,12 @@
 ///! Simple structures for interoperability with dense matrices
 
 use std::ops::{Deref, DerefMut};
+use std::iter::Map;
+use std::slice::{Chunks, ChunksMut};
 use num::traits::Num;
 use dense_mats::{DenseMatView, DenseMatViewMut, StorageOrder,
                  DenseMatOwned,};
+use errors::SprsError;
 
 /// A simple dense matrix
 pub struct DMat<N, Storage>
@@ -16,6 +19,17 @@ where Storage: Deref<Target=[N]> {
 
 pub type DMatView<'a, N> = DMat<N, &'a [N]>;
 pub type DMatOwned<N> = DMat<N, Vec<N>>;
+
+/// A simple dense vector
+pub struct DVec<N, Storage>
+where Storage: Deref<Target=[N]> {
+    data: Storage,
+    dim: usize,
+    stride: usize,
+}
+
+pub type DVecView<'a, N> = DVec<N, &'a [N]>;
+pub type DVecOwned<N> = DVec<N, Vec<N>>;
 
 impl<N, Storage> DenseMatView<N> for DMat<N, Storage>
 where Storage: Deref<Target=[N]> {
@@ -105,5 +119,61 @@ where Storage: Deref<Target=[N]> {
         }
     }
 
+    /// Get a view into the specified row
+    pub fn row(&self, i: usize) -> Result<DVecView<N>, SprsError> {
+        if i > self.rows {
+            return Err(SprsError::OutOfBoundsIndex);
+        }
+        let start = i * self.strides[0];
+        let stop = (i + 1) * self.strides[0];
+        let data = &self.data[start..stop];
+        Ok(DVec {
+            data: data,
+            dim: self.cols,
+            stride: self.strides[1],
+        })
+    }
+
+    /// Get a view into the specified column
+    pub fn col(&self, j: usize) -> Result<DVecView<N>, SprsError> {
+        if j > self.cols {
+            return Err(SprsError::OutOfBoundsIndex);
+        }
+        let start = j * self.strides[1];
+        let stop = (j + 1) * self.strides[1];
+        let data = &self.data[start..stop];
+        Ok(DVec {
+            data: data,
+            dim: self.cols,
+            stride: self.strides[0],
+        })
+    }
+}
+
+fn take_first<N>(chunk: &[N]) -> &N {
+    &chunk[0]
+}
+
+fn take_first_mut<N>(chunk: &mut [N]) -> &mut N {
+    &mut chunk[0]
+}
+
+
+impl<N, Storage> DVec<N, Storage>
+where Storage: Deref<Target=[N]> {
+
+    /// Iterate over a dense vector's values by reference
+    pub fn iter(&self) -> Map<Chunks<N>, fn(&[N]) -> &N> {
+        self.data.chunks(self.stride).map(take_first)
+    }
+}
+
+impl<N, Storage> DVec<N, Storage>
+where Storage: DerefMut<Target=[N]> {
+
+    /// Iterate over a dense vector's values by mutable reference
+    pub fn iter_mut(&mut self) -> Map<ChunksMut<N>, fn(&mut [N]) -> &mut N> {
+        self.data.chunks_mut(self.stride).map(take_first_mut)
+    }
 }
 
