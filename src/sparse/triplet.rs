1669627111ea709a3fff92e0ccc0baf473334707
Vincent Barrielle <vincent.barrielle@m4x.org>
2015-12-09 18:22:35 +0100

    add more utility functions to tripler matrix

--- src/sparse/triplet.rs
+++ src/sparse/triplet.rs
@@ -12,39 +12,84 @@ pub struct TripletMat<N> {
 
 impl<N> TripletMat<N> {
 
-    pub fn new(rows: usize, cols: usize) -> TripletMat<N> {
+    pub fn new(shape: (usize, usize)) -> TripletMat<N> {
         TripletMat {
-            rows: rows,
-            cols: cols,
+            rows: shape.0,
+            cols: shape.1,
             row_inds: Vec::new(),
             col_inds: Vec::new(),
             data: Vec::new(),
         }
     }
 
-    pub fn with_capacity(rows: usize,
-                         cols: usize,
+    pub fn with_capacity(shape: (usize, usize),
                          cap: usize)
                          -> TripletMat<N> {
         TripletMat {
-            rows: rows,
-            cols: cols,
+            rows: shape.0,
+            cols: shape.1,
             row_inds: Vec::with_capacity(cap),
             col_inds: Vec::with_capacity(cap),
             data: Vec::with_capacity(cap),
         }
     }
 
+    pub fn from_triplets(shape: (usize, usize),
+                         row_inds: Vec<usize>,
+                         col_inds: Vec<usize>,
+                         data: Vec<N>)
+                         -> TripletMat<N> {
+        assert!(row_inds.len() == col_inds.len(),
+                "all inputs should have the same length");
+        assert!(data.len() == col_inds.len(),
+                "all inputs should have the same length");
+        assert!(row_inds.len() == data.len(),
+                "all inputs should have the same length");
+        TripletMat {
+            rows: shape.0,
+            cols: shape.1,
+            row_inds: row_inds,
+            col_inds: col_inds,
+            data: data,
+        }
+    }
+
     pub fn rows(&self) -> usize {
-        self.rows
+        self.borrowed().rows()
     }
 
     pub fn cols(&self) -> usize {
-        self.cols
+        self.borrowed().cols()
+    }
+
+    pub fn shape(&self) -> (usize, usize) {
+        self.borrowed().shape()
     }
 
     pub fn nnz(&self) -> usize {
-        self.data.len()
+        self.borrowed().nnz()
+    }
+
+    pub fn row_inds(&self) -> &[usize] {
+        self.borrowed().row_inds()
+    }
+
+    pub fn col_inds(&self) -> &[usize] {
+        self.borrowed().col_inds()
+    }
+
+    pub fn data(&self) -> &[N] {
+        self.borrowed().data()
+    }
+
+    pub fn borrowed(&self) -> TripletView<N> {
+        TripletView {
+            rows: self.rows,
+            cols: self.cols,
+            row_inds: &self.row_inds[..],
+            col_inds: &self.col_inds[..],
+            data: &self.data[..],
+        }
     }
 
     pub fn add_triplet(&mut self, row: usize, col: usize, val: N) {
@@ -63,5 +108,44 @@ impl<N> TripletMat<N> {
         self.row_inds.reserve_exact(cap);
         self.col_inds.reserve_exact(cap);
         self.data.reserve_exact(cap);
+    }
+}
+
+/// Triplet matrix view
+pub struct TripletView<'a, N: 'a> {
+    rows: usize,
+    cols: usize,
+    row_inds: &'a [usize],
+    col_inds: &'a [usize],
+    data: &'a [N],
+}
+
+impl<'a, N> TripletView<'a, N> {
+    pub fn rows(&self) -> usize {
+        self.rows
+    }
+
+    pub fn cols(&self) -> usize {
+        self.cols
+    }
+
+    pub fn shape(&self) -> (usize, usize) {
+        (self.rows, self.cols)
+    }
+
+    pub fn nnz(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn row_inds(& self) -> &'a [usize] {
+        self.row_inds
+    }
+
+    pub fn col_inds(& self) -> &'a [usize] {
+        self.col_inds
+    }
+
+    pub fn data(&self) -> &'a [N] {
+        self.data
     }
 }
