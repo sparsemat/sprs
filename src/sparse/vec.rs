edb17520a2eb9d97f0427ce92e8ca832803613c1
Eduard Bopp <eduard.bopp@aepsil0n.de>
2017-10-27 21:27:09 +0200

    Implement additive traits up to group

--- src/sparse/vec.rs
+++ src/sparse/vec.rs
@@ -1016,6 +1016,12 @@ mod alga_impls {
         }
     }
 
+    impl<N: Copy + Num + Neg<Output=N>, I: SpIndex> AbstractQuasigroup<Additive> for CsVecI<N, I> {}
+
+    impl<N: Copy + Num + Neg<Output=N>, I: SpIndex> AbstractLoop<Additive> for CsVecI<N, I> {}
+
+    impl<N: Copy + Num + Neg<Output=N>, I: SpIndex> AbstractGroup<Additive> for CsVecI<N, I> {}
+
     #[cfg(test)]
     mod test {
         use super::*;
