a228f160a1c211e728a7806c67486c4710b89b36
Vincent Barrielle <vincent.barrielle@m4x.org>
2018-03-26 22:51:48 +0200

    implement sketch of matrix collection testing logic
    Testing on the example file revealed that the current parser cannot
    read the bundle_adj collection entry, this probably means a bug in our
    parser.

--- integration_tests/cholesky/src/main.rs
+++ integration_tests/cholesky/src/main.rs
@@ -8,21 +8,66 @@ extern crate tar;
 
 use tempdir::TempDir;
 use std::path::Path;
-use std::io;
 use std::fs::File;
+use sprs::{CsMat};
+
+type GenError = Box<std::error::Error>;
+
+fn try_url<P>(mx_url: &str,
+              save_dir: P,
+              mat_name: &str,
+              rhs_name: &str) -> Result<(), GenError>
+where P: AsRef<Path>
+{
+    let save_dir = save_dir.as_ref();
+    let mut archive_name = Path::new(mx_url);
+    while let Some(stem) = archive_name.file_stem() {
+        if archive_name != stem {
+            archive_name = Path::new(stem);
+        } else {
+            break;
+        }
+    }
+    let extracted_path = save_dir.join(archive_name);
+    let probe = save_dir.join(&extracted_path).join("dl_done");
+    if !probe.exists() {
+        println!("probe {} does not exist, downloading {}",
+                 probe.to_string_lossy(),
+                 mx_url);
+        tar::Archive::new(
+            libflate::gzip::Decoder::new(
+                reqwest::get(mx_url)?
+            )?
+        ).unpack(save_dir)?;
+        let _ = File::create(probe)?;
+    } else {
+        println!("Using cached archive {}", extracted_path.to_string_lossy());
+    }
+
+    let sys_mat_path = extracted_path.join(mat_name);
+    let rhs_path = extracted_path.join(rhs_name);
+    let sys_mat: CsMat<f64> = sprs::io::read_matrix_market(sys_mat_path)?
+        .to_csc();
+    let _sys_rhs: CsMat<f64> = sprs::io::read_matrix_market(rhs_path)?
+        .to_csc();
+
+    let _ldl = sprs_suitesparse_ldl::LdlNumeric::new(sys_mat.view());
+
+    Ok(())
+}
 
 fn main() {
     let mx_url = "https://sparse.tamu.edu/MM/Mazaheri/bundle_adj.tar.gz";
     let tmp_dir = TempDir::new("sprs-chol-tmp").unwrap();
-    let save_path = tmp_dir.path()
-                           .join(Path::new(mx_url).file_name().unwrap());
+    let _save_path = tmp_dir.path()
+                            .join(Path::new(mx_url).file_name().unwrap());
     let save_dir = "/tmp/";
 
-    // FIXME better handle errors
-    tar::Archive::new(
-        libflate::gzip::Decoder::new(
-            reqwest::get(mx_url).expect("cannot parse matrix url")
-        ).expect("not a valid gz file")
-    ).unpack(save_dir).expect("not a valid tar file");
+    let mat_name = "bundle_adj.mtx";
+    let rhs_name = "bundle_adj_b.mtx";
+
+    if let Err(error) = try_url(mx_url, save_dir, mat_name, rhs_name) {
+        println!("error: {} while trying url {}", error, mx_url);
+    }
 }
 
