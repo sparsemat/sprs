0ae280ff4246ce78a99650a9bc27fcb4820e6981
Vincent Barrielle <vincent.barrielle@m4x.org>
2019-12-20 12:51:39 +0100

    Enable visualization of nnz patterns as png images in example code

--- examples/fill_in_reduction.rs
+++ examples/fill_in_reduction.rs
@@ -1,11 +1,14 @@
+extern crate image;
 extern crate ndarray;
 ///! This file demonstrates how it is possible to reduce the fill-in of a
 ///! symmetric sparse matrix during Cholesky decomposition.
 extern crate sprs;
 extern crate tobj;
 
+use image::{ImageBuffer, ImageError, ImageResult, Luma};
 use ndarray::{arr2, ArrayView2};
 use std::env;
+use std::error::Error;
 use std::path::Path;
 
 fn small_lap_mat() -> sprs::CsMat<f64> {
@@ -52,7 +55,24 @@ fn lap_mat_from_obj(path: &str) -> Result<sprs::CsMat<f64>, tobj::LoadError> {
     Err(tobj::LoadError::ReadError)
 }
 
-fn main() -> Result<(), tobj::LoadError> {
+fn save_gray_image(
+    path: impl AsRef<Path>,
+    image: ArrayView2<u8>,
+) -> ImageResult<()> {
+    let height = image.shape()[0];
+    let width = image.shape()[1];
+    let im: Option<ImageBuffer<Luma<u8>, _>> = image.to_slice().map(|slice| {
+        ImageBuffer::from_raw(width as u32, height as u32, slice)
+            .expect("failed to create image from slice")
+    });
+    let im = im.ok_or(ImageError::FormatError(
+        "non-contiguous ndarray Array".to_owned(),
+    ))?;
+    im.save(path)?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let mut args = env::args();
     args.next();
     let lap_mat = if let Some(path) = args.next() {
@@ -61,17 +81,35 @@ fn main() -> Result<(), tobj::LoadError> {
         small_lap_mat()
     };
 
-    println!(
-        "Lap mat nnz pattern:\n\n{}",
-        sprs::visu::nnz_pattern_formatter(lap_mat.view()),
-    );
-
     let ordering = sprs::linalg::cuthill_mckee(lap_mat.view());
     let perm_lap =
         sprs::transform_mat_papt(lap_mat.view(), ordering.perm.view());
-    println!(
-        "After Cuthill-McKee, profile is reduced:\n\n{}",
-        sprs::visu::nnz_pattern_formatter(perm_lap.view()),
-    );
+
+    if let Some(im_folder) = args.next() {
+        println!(
+            "saving nnz patterns in {}/pattern_{{before,after}}.png",
+            im_folder,
+        );
+        let im_folder = Path::new(&im_folder);
+        save_gray_image(
+            im_folder.join("pattern_before.png"),
+            sprs::visu::nnz_image(lap_mat.view()).view(),
+        )?;
+
+        save_gray_image(
+            im_folder.join("pattern_after.png"),
+            sprs::visu::nnz_image(perm_lap.view()).view(),
+        )?;
+    } else {
+        println!(
+            "Lap mat nnz pattern:\n\n{}",
+            sprs::visu::nnz_pattern_formatter(lap_mat.view()),
+        );
+        println!(
+            "After Cuthill-McKee, profile is reduced:\n\n{}",
+            sprs::visu::nnz_pattern_formatter(perm_lap.view()),
+        );
+    }
+
     Ok(())
 }
