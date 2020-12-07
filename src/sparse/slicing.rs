45a2bff635adb16add8e324fcbaed37fddad2674
Vincent Barrielle <vincent.barrielle@m4x.org>
2020-12-07 22:33:26 +0100

    Add ergonomic middle_slice functions for `CsMatBase` variants
    To be able to extend the lifetime of the `middle_slice` method targeting
    `CsMatViewI` without having to use an other name, I'm experimenting a
    new approach, where I re-implement the method for each main variant. Of
    course I'd still like to be able to write a single implementation, but I
    guess only a future version of rust, with specialization, or maybe
    associated type constructors, will be able to help.

---
+++ src/sparse/slicing.rs
@@ -0,0 +1,122 @@
+//! This module implementations to slice a matrix along the desired dimension.
+//! We're using a sealed trait to enable using ranges for an idiomatic API.
+
+use crate::range::Range;
+
+impl<N, I, Iptr> crate::CsMatI<N, I, Iptr>
+where
+    I: crate::SpIndex,
+    Iptr: crate::SpIndex,
+{
+    /// Slice the outer dimension of the matrix according to the specified
+    /// range.
+    pub fn slice_outer<S>(&self, range: S) -> crate::CsMatViewI<N, I, Iptr>
+    where
+        S: Range,
+    {
+        let start = range.start().unwrap_or(0);
+        let end = range.end().unwrap_or(self.outer_dims());
+        if end < start {
+            panic!("Invalid view");
+        }
+        let outer_inds_slice = self.indptr.outer_inds_slice(start, end);
+        let (nrows, ncols) = match self.storage() {
+            crate::CSR => ((end - start), self.ncols),
+            crate::CSC => (self.nrows, (end - start)),
+        };
+        crate::CsMatViewI {
+            nrows,
+            ncols,
+            storage: self.storage,
+            indptr: self.indptr.middle_slice(range),
+            indices: &self.indices[outer_inds_slice.clone()],
+            data: &self.data[outer_inds_slice],
+        }
+    }
+}
+
+impl<'a, N, I, Iptr> crate::CsMatViewI<'a, N, I, Iptr>
+where
+    I: crate::SpIndex,
+    Iptr: crate::SpIndex,
+{
+    /// Slice the outer dimension of the matrix according to the specified
+    /// range.
+    pub fn slice_outer<S>(&self, range: S) -> crate::CsMatViewI<'a, N, I, Iptr>
+    where
+        S: Range,
+    {
+        let start = range.start().unwrap_or(0);
+        let end = range.end().unwrap_or(self.outer_dims());
+        if end < start {
+            panic!("Invalid view");
+        }
+        let outer_inds_slice = self.indptr.outer_inds_slice(start, end);
+        let (nrows, ncols) = match self.storage() {
+            crate::CSR => ((end - start), self.ncols),
+            crate::CSC => (self.nrows, (end - start)),
+        };
+        crate::CsMatViewI {
+            nrows,
+            ncols,
+            storage: self.storage,
+            indptr: self.indptr.middle_slice(range),
+            indices: &self.indices[outer_inds_slice.clone()],
+            data: &self.data[outer_inds_slice],
+        }
+    }
+}
+
+impl<'a, N, I, Iptr> crate::CsMatViewMutI<'a, N, I, Iptr>
+where
+    I: crate::SpIndex,
+    Iptr: crate::SpIndex,
+{
+    /// Slice the outer dimension of the matrix according to the specified
+    /// range.
+    pub fn slice_outer<S>(
+        &mut self,
+        range: S,
+    ) -> crate::CsMatViewMutI<N, I, Iptr>
+    where
+        S: Range,
+    {
+        let start = range.start().unwrap_or(0);
+        let end = range.end().unwrap_or(self.outer_dims());
+        if end < start {
+            panic!("Invalid view");
+        }
+        let outer_inds_slice = self.indptr.outer_inds_slice(start, end);
+        let (nrows, ncols) = match self.storage() {
+            crate::CSR => ((end - start), self.ncols),
+            crate::CSC => (self.nrows, (end - start)),
+        };
+        crate::CsMatViewMutI {
+            nrows,
+            ncols,
+            storage: self.storage,
+            indptr: self.indptr.middle_slice(range),
+            indices: &self.indices[outer_inds_slice.clone()],
+            data: &mut self.data[outer_inds_slice],
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::CsMat;
+
+    #[test]
+    fn slice_outer() {
+        let size = 11;
+        let csr: CsMat<f64> = CsMat::eye(size);
+        let sliced = csr.view().slice_outer(2..7);
+        let mut iter = sliced.into_iter();
+        assert_eq!(iter.next().unwrap(), (&1., (0, 2)));
+        assert_eq!(iter.next().unwrap(), (&1., (1, 3)));
+        assert_eq!(iter.next().unwrap(), (&1., (2, 4)));
+        assert_eq!(iter.next().unwrap(), (&1., (3, 5)));
+        assert_eq!(iter.next().unwrap(), (&1., (4, 6)));
+        assert!(iter.next().is_none());
+    }
+}
