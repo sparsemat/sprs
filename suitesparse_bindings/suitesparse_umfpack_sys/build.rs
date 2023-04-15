932be22743f2a45c8a430136a2cedf82cd667757
James Logan <jlogan03@gmail.com>
2023-04-15 12:30:43 -0400

    add build.rs. add configuration for static linkage to cargo config.

---
+++ suitesparse_bindings/suitesparse_umfpack_sys/build.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    if std::env::var_os("CARGO_FEATURE_STATIC").is_some() {
+        let path_to_umfpack = std::env::var("DEP_SUITESPARSE_SRC_ROOT").unwrap();
+        println!("cargo:rustc-link-search=native={path_to_umfpack}");
+        println!("cargo:rustc-link-lib=static=umfpack");
+        println!("cargo:rustc-link-lib=static=suitesparseconfig");
+    } else {
+        println!("cargo:rustc-link-lib=umfpack");
+    }
+}
