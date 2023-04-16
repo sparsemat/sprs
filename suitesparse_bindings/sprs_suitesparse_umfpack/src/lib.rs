6326a6cfc23b8d5dc42be303475e7740986fc00e
James Logan <jlogan03@gmail.com>
2023-04-16 18:32:29 -0400

    add function for doing the solve

--- suitesparse_bindings/sprs_suitesparse_umfpack/src/lib.rs
+++ suitesparse_bindings/sprs_suitesparse_umfpack/src/lib.rs
@@ -23,15 +23,16 @@ macro_rules! umfpack_impl {
         struct $Numeric(*const c_void);
 
         pub struct $Context {
+            mat: CsMatI<f64, $int>,
             symbolic: $Symbolic,
             numeric: $Numeric,
             nrow: usize,
             ncol: usize,
-            nnz: usize
+            nnz: usize,
         }
 
         impl $Context {
-            pub unsafe fn new<N>(mat: &CsMatI<N, $int>) -> Self 
+            pub unsafe fn new<N>(mat: CsMatI<N, $int>) -> Self
             where N: Default + Clone + Into<f64>,
             {
                 // UMFPACK methods require CSC format and f64 data
@@ -79,12 +80,45 @@ macro_rules! umfpack_impl {
                 let numeric = $Numeric(*numeric_inner);
 
                 Self {
+                    mat: mat,
                     symbolic: symbolic,
                     numeric: numeric,
                     nrow: nrow,
                     ncol: ncol,
                     nnz: nnz
                 }
+            }
+
+            pub unsafe fn solve(&self, b: &[f64]) -> Vec<f64> {
+                // Check shape
+                let n = self.nrow * self.ncol;
+                assert!(b.len() == n, "Input right-hand-side does not have the expected number of entries");
+                assert!(self.nrow == self.ncol, "Solve can only be performed for square systems");
+
+                // Allocate dense output vector
+                let mut x = vec![0.0; n];
+
+                // Get C-compatible raw pointers to column pointers, indices, and values
+                let ap = self.mat.indptr().to_proper().as_ptr();
+                let ai = self.mat.indices().as_ptr();
+                let ax = self.mat.data().as_ptr();
+
+                // Do the linear solve using pre-computed LU factors and permutation
+                unsafe {
+                    $solve(
+                        0,  // sys=0 for Ax=b problem type
+                        ap,
+                        ai,
+                        ax,
+                        x.as_mut_ptr(),
+                        b.as_ptr(),
+                        self.numeric.0,
+                        0 as *const f64,  // Default settings
+                        0 as *mut c_void  // Ignore info
+                    )
+                };
+
+                x
             }
         }
      };
