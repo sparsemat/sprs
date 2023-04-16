fde172da0f6dde9e568ab85407bdf804ee124c07
James Logan <jlogan03@gmail.com>
2023-04-15 23:59:41 -0400

    split umfpack_solve

--- suitesparse_bindings/suitesparse_umfpack_sys/src/umfpack_symbolic.rs
+++ suitesparse_bindings/suitesparse_umfpack_sys/src/umfpack_symbolic.rs
@@ -1,9 +1,23 @@
 use libc::{c_double, c_void};
-use super::SuiteSparseLong;
+use super::{SuiteSparseLong, SuiteSparseInt};
 
 // Define a C function signature for umfpack_symbolic
 extern "C" {
-    fn umfpack_symbolic(
+    fn umfpack_di_symbolic(
+        n_row: SuiteSparseInt,
+        n_col: SuiteSparseInt,
+        Ap: *const SuiteSparseInt,
+        Ai: *const SuiteSparseInt,
+        Ax: *const c_double,
+        Symbolic: *mut *mut c_void,
+        Control: *const c_double,
+        Info: *mut c_void,
+    ) -> SuiteSparseInt;
+}
+
+// Define a C function signature for umfpack_symbolic
+extern "C" {
+    fn umfpack_dl_symbolic(
         n_row: SuiteSparseLong,
         n_col: SuiteSparseLong,
         Ap: *const SuiteSparseLong,
@@ -16,7 +30,32 @@ extern "C" {
 }
 
 // Define a Rust wrapper function for umfpack_symbolic
-pub fn umfpack_symbolic_wrapper(
+pub fn umfpack_di_symbolic_wrapper(
+    n_row: SuiteSparseInt,               // Number of rows
+    n_col: SuiteSparseInt,               // Number of columns
+    ap: &[SuiteSparseInt],               // Column pointers for sparse matrix A
+    ai: &[SuiteSparseInt],               // Row indices for sparse matrix A
+    ax: &[c_double],            // Values for sparse matrix A
+    symbolic: &mut *mut c_void, // Opaque representation of symbolic decomposition; not populated yet
+    control: &[c_double], // Control parameters; null pointer -> default settings
+    info: &mut [SuiteSparseInt],   // Info readout; null pointer -> ignore readout
+) -> SuiteSparseInt {
+    unsafe {
+        umfpack_di_symbolic(
+            n_row,
+            n_col,
+            ap.as_ptr(),
+            ai.as_ptr(),
+            ax.as_ptr(),
+            symbolic as *mut *mut c_void,
+            control.as_ptr(),
+            info.as_mut_ptr() as *mut c_void,
+        )
+    }
+}
+
+// Define a Rust wrapper function for umfpack_symbolic
+pub fn umfpack_dl_symbolic_wrapper(
     n_row: SuiteSparseLong,               // Number of rows
     n_col: SuiteSparseLong,               // Number of columns
     ap: &[SuiteSparseLong],               // Column pointers for sparse matrix A
@@ -27,7 +66,7 @@ pub fn umfpack_symbolic_wrapper(
     info: &mut [SuiteSparseLong],   // Info readout; null pointer -> ignore readout
 ) -> SuiteSparseLong {
     unsafe {
-        umfpack_symbolic(
+        umfpack_dl_symbolic(
             n_row,
             n_col,
             ap.as_ptr(),
