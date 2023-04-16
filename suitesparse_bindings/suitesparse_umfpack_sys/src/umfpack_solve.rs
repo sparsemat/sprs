f50d7d71fa9dc4def7661e681da0ea579efb8271
James Logan <jlogan03@gmail.com>
2023-04-15 23:58:12 -0400

    split umfpack_solve

--- suitesparse_bindings/suitesparse_umfpack_sys/src/umfpack_solve.rs
+++ suitesparse_bindings/suitesparse_umfpack_sys/src/umfpack_solve.rs
@@ -1,9 +1,24 @@
 use libc::{c_double, c_void};
-use super::SuiteSparseLong;
+use super::{SuiteSparseLong, SuiteSparseInt};
 
 // Define the C function signature for umfpack_solve
 extern "C" {
-    fn umfpack_solve(
+    fn umfpack_di_solve(
+        sys: SuiteSparseInt,
+        Ap: *const SuiteSparseInt,
+        Ai: *const SuiteSparseInt,
+        Ax: *const c_double,
+        X: *mut c_double,
+        B: *const c_double,
+        Numeric: *const c_void,
+        Control: *const c_double,
+        Info: *mut c_void,
+    ) -> SuiteSparseInt;
+}
+
+// Define the C function signature for umfpack_solve
+extern "C" {
+    fn umfpack_dl_solve(
         sys: SuiteSparseLong,
         Ap: *const SuiteSparseLong,
         Ai: *const SuiteSparseLong,
@@ -17,7 +32,34 @@ extern "C" {
 }
 
 // Define a Rust wrapper function for umfpack_solve
-pub fn umfpack_solve_wrapper(
+pub fn umfpack_di_solve_wrapper(
+    sys: SuiteSparseInt,
+    ap: &[SuiteSparseInt],           // Column pointers for sparse matrix A
+    ai: &[SuiteSparseInt],           // Row indices for sparse matrix A
+    ax: &[c_double],        // Values for sparse matrix A
+    x: &mut [c_double],     // Values for Ax=b system x
+    b: &[c_double], // Values for Ax=b system b, to be populated by solve
+    numeric: *const c_void, // Opaque representation of LU decomposition; populated by umfpack_numeric
+    control: &[c_double], // Control parameters; null pointer -> default settings
+    info: &mut [SuiteSparseInt],   // Info readout; null pointer -> ignore readout
+) -> SuiteSparseInt {
+    unsafe {
+        umfpack_di_solve(
+            sys,
+            ap.as_ptr(),
+            ai.as_ptr(),
+            ax.as_ptr(),
+            x.as_mut_ptr(),
+            b.as_ptr(),
+            numeric,
+            control.as_ptr(),
+            info.as_mut_ptr() as *mut c_void,
+        )
+    }
+}
+
+// Define a Rust wrapper function for umfpack_solve
+pub fn umfpack_dl_solve_wrapper(
     sys: SuiteSparseLong,
     ap: &[SuiteSparseLong],           // Column pointers for sparse matrix A
     ai: &[SuiteSparseLong],           // Row indices for sparse matrix A
@@ -29,7 +71,7 @@ pub fn umfpack_solve_wrapper(
     info: &mut [SuiteSparseLong],   // Info readout; null pointer -> ignore readout
 ) -> SuiteSparseLong {
     unsafe {
-        umfpack_solve(
+        umfpack_dl_solve(
             sys,
             ap.as_ptr(),
             ai.as_ptr(),
