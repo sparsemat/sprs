947bb7506095ebee42a82d99a9042b2ee44aeeed
James Logan <jlogan03@gmail.com>
2023-04-16 09:53:12 -0400

    add long version of get_numeric

--- suitesparse_bindings/suitesparse_umfpack_sys/src/umfpack_get_numeric.rs
+++ suitesparse_bindings/suitesparse_umfpack_sys/src/umfpack_get_numeric.rs
@@ -1,22 +1,6 @@
 use super::{SuiteSparseInt, SuiteSparseLong};
 use libc::{c_double, c_void};
 
-// int umfpack_di_get_numeric
-// (
-//     int Lp [ ],
-//     int Lj [ ],
-//     double Lx [ ],
-//     int Up [ ],
-//     int Ui [ ],
-//     double Ux [ ],
-//     int P [ ],
-//     int Q [ ],
-//     double Dx [ ],
-//     int *do_recip,
-//     double Rs [ ],
-//     void *Numeric
-// ) ;
-
 extern "C" {
     fn umfpack_di_get_numeric(
         Lp: *mut SuiteSparseInt,
@@ -32,6 +16,21 @@ extern "C" {
         Rs: *mut c_double,
         Numeric: *const c_void,
     ) -> SuiteSparseInt;
+
+    fn umfpack_dl_get_numeric(
+        Lp: *mut SuiteSparseLong,
+        Lj: *mut SuiteSparseLong,
+        Lx: *mut c_double,
+        Up: *mut SuiteSparseLong,
+        Ui: *mut SuiteSparseLong,
+        Ux: *mut c_double,
+        P: *mut SuiteSparseLong,
+        Q: *mut SuiteSparseLong,
+        Dx: *mut c_double,
+        do_recip: *mut SuiteSparseLong,
+        Rs: *mut c_double,
+        Numeric: *const c_void,
+    ) -> SuiteSparseLong;
 }
 
 pub fn umfpack_di_get_numeric_wrapper(
@@ -60,6 +59,38 @@ pub fn umfpack_di_get_numeric_wrapper(
             q.as_mut_ptr(),
             dx.as_mut_ptr(),
             do_recip as *mut SuiteSparseInt,
+            rs.as_mut_ptr(),
+            numeric,
+        )
+    }
+}
+
+pub fn umfpack_dl_get_numeric_wrapper(
+    lp: &mut [SuiteSparseLong],
+    lj: &mut [SuiteSparseLong],
+    lx: &mut [c_double],
+    up: &mut [SuiteSparseLong],
+    ui: &mut [SuiteSparseLong],
+    ux: &mut [c_double],
+    p: &mut [SuiteSparseLong],
+    q: &mut [SuiteSparseLong],
+    dx: &mut [c_double],
+    do_recip: &mut SuiteSparseLong,
+    rs: &mut [c_double],
+    numeric: *const c_void,
+) -> SuiteSparseLong {
+    unsafe {
+        umfpack_dl_get_numeric(
+            lp.as_mut_ptr(),
+            lj.as_mut_ptr(),
+            lx.as_mut_ptr(),
+            up.as_mut_ptr(),
+            ui.as_mut_ptr(),
+            ux.as_mut_ptr(),
+            p.as_mut_ptr(),
+            q.as_mut_ptr(),
+            dx.as_mut_ptr(),
+            do_recip as *mut SuiteSparseLong,
             rs.as_mut_ptr(),
             numeric,
         )
